e8db9468d8ea88b29ed8d6fb3d343168b5bd0ab7
Guillaume Racicot <gufideg@gmail.com>
2018-02-28 23:00:05 -0500

    Added backend

--- entities.py
+++ entities.py
@@ -6,36 +6,42 @@ db = orm.Database();
 class AuthentificationEmploye(db.Entity):
 	id_authentification = orm.PrimaryKey(int, auto=True)
 	matricule = orm.Required(str, unique=True)
-	mot_de_masse = orm.Required(str)
+	mot_de_passe = orm.Required(str)
+	employe = orm.Optional('Employe')
 
 class AuthentificationClient(db.Entity):
 	id_authentification = orm.PrimaryKey(int, auto=True)
 	courriel = orm.Required(str, unique=True)
-	mot_de_masse = orm.Required(str)
+	mot_de_passe = orm.Required(str)
+	client = orm.Optional('Client')
 
 class Localisation(db.Entity):
 	id_localisation = orm.PrimaryKey(int, auto=True)
 	ville = orm.Required(str)
 	province = orm.Required(str)
+	adresses = orm.Set('Adresse')
 
 class Adresse(db.Entity):
 	id_adresse = orm.PrimaryKey(int, auto=True)
-	localisation = orm.Required(Localisation)
+	id_localisation = orm.Required(Localisation)
 	adresse_civique = orm.Required(str)
 	code_postal = orm.Required(str)
+	dossier = orm.Optional('Dossier')
 
 class Dossier(db.Entity):
 	id_dossier = orm.PrimaryKey(int, auto=True)
-	adresse = orm.Required(Adresse)
+	id_adresse = orm.Required(Adresse)
 	nom = orm.Required(str)
 	prenom = orm.Required(str)
 	num_telephone = orm.Required(str)
 	date_naissance = orm.Required(date)
+	employe = orm.Optional('Employe')
+	client = orm.Optional('Client')
 
 class Employe(db.Entity):
 	id_employe = orm.PrimaryKey(int, auto=True)
-	dossier = orm.Required(Dossier)
-	authentification = orm.Required(AuthentificationEmploye)
+	id_dossier = orm.Required(Dossier)
+	id_authentification = orm.Required(AuthentificationEmploye)
 
 class Forfait(db.Entity):
 	id_forfait = orm.PrimaryKey(int, auto=True)
@@ -43,25 +49,29 @@ class Forfait(db.Entity):
 	cout = orm.Required(int)
 	max_location = orm.Required(int)
 	max_duree = orm.Required(int)
+	abonnement = orm.Set('Abonnement')
 
 class Abonnement(db.Entity):
 	id_abonnement = orm.PrimaryKey(int, auto=True)
-	forfait = orm.Required(Forfait)
+	id_forfait = orm.Required(Forfait)
 	date_debut = orm.Required(date)
 	date_fin = orm.Required(date)
+	client = orm.Optional('Client')
 
 class CarteCredit(db.Entity):
 	id_carte = orm.PrimaryKey(int, auto=True)
 	numero = orm.Required(str)
 	cvv = orm.Required(str)
 	date_expiration = orm.Required(date)
+	client = orm.Optional('Client')
 
 class Client(db.Entity):
 	id_client = orm.PrimaryKey(int, auto=True)
-	dossier = orm.Required(Dossier)
-	carte = orm.Required(CarteCredit)
-	authentification = orm.Required(AuthentificationClient)
-	abonnement = orm.Required(Abonnement)
+	id_dossier = orm.Required(Dossier)
+	id_carte = orm.Required(CarteCredit)
+	id_authentification = orm.Required(AuthentificationClient)
+	id_abonnement = orm.Required(Abonnement)
+	locations = orm.Set('Location')
 
 class Genre(db.Entity):
 	id_genre = orm.PrimaryKey(int, auto=True)
@@ -76,35 +86,54 @@ class Pays(db.Entity):
 class Personne(db.Entity):
 	id_personne = orm.PrimaryKey(int, auto=True)
 	nom = orm.Required(str)
-	date_naissance = orm.Required(date)
-	lieu_naissance = orm.Required(str)
-	bio = orm.Required(str)
+	date_naissance = orm.Optional(date)
+	lieu_naissance = orm.Optional(str)
+	bio = orm.Optional(str)
+	films = orm.Set('Film', reverse='id_realisateur')
+	roles = orm.Set('Role')
+	film_scenarist = orm.Set('Film' , reverse='scenaristes')
 
 class Film(db.Entity):
 	id_film = orm.PrimaryKey(int, auto=True)
-	realisateur = orm.Required(Personne)
+	id_realisateur = orm.Optional(Personne)
 	titre = orm.Required(str)
 	annee_sortie = orm.Required(date)
 	duree = orm.Required(int)
-	langue_originale = orm.Required(str)
+	langue_originale = orm.Optional(str)
 	resume = orm.Required(str)
 	genre = orm.Set(Genre)
 	pays = orm.Set(Pays)
+	roles = orm.Set('Role')
+	copies = orm.Set('Copie')
+	scenaristes = orm.Set(Personne)
 
 class Role(db.Entity):
-	film = orm.Required(Film)
-	acteur = orm.Required(Personne)
+	id_film = orm.Required(Film)
+	id_personne = orm.Required(Personne)
 	titre = orm.Required(str)
+	orm.PrimaryKey(id_film, id_personne)
 
 class Copie(db.Entity):
 	id_copie = orm.PrimaryKey(int, auto=True)
-	film = orm.Required(Film)
+	id_film = orm.Required(Film)
 	code = orm.Required(str)
+	locations = orm.Set('Location')
 
 class Location(db.Entity):
 	id_location = orm.PrimaryKey(int, auto=True)
-	client = orm.Required(Client)
-	copie = orm.Required(Copie)
+	id_client = orm.Required(Client)
+	id_copie = orm.Required(Copie)
 	debut = orm.Required(date)
 	fin = orm.Required(date)
 	remise = orm.Optional(date)
+
+def init():
+	db.bind(provider='sqlite', filename='./db/Labo2.db')
+	db.generate_mapping(create_tables=False)
+	with db.set_perms_for(Film):
+		orm.perm('edit create delete view', group='anybody')
+	with db.set_perms_for(Role):
+		orm.perm('edit create delete view', group='anybody')
+	with db.set_perms_for(Personne):
+		orm.perm('edit create delete view', group='anybody')
+
--- main.py
+++ main.py
@@ -1,12 +1,102 @@
 #!/usr/bin/python
 
-from bottle import Bottle, run
+from bottle import Bottle, run, get, post, request
 import entities
+import time
+from datetime import date
+from pony import orm
+import json
 
 app = Bottle()
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+entities.init()
 
-run(app, host='localhost', port=8080)
+@app.post('/login')
+@orm.db_session
+def login():
+	from bottle import response
+	email = request.json['identifiant']
+	password = request.json['motDePasse']
+	
+	client = orm.select(c.client for c in entities.AuthentificationClient if c.courriel == email and c.mot_de_passe == password).first()
+	
+	if(client != None):
+		films = orm.select(f for f in entities.Film)[:]
+		acteurs = orm.select(a.id_personne for a in entities.Role)[:]
+		personnes = orm.select(p for p in entities.Personne)[:]
+		
+		response.content_type = 'application/json'
+		return json.JSONEncoder().encode({
+			'client':{
+				'id_client': client.id_client,
+				'nom': client.id_dossier.prenom + ' ' + client.id_dossier.nom,
+				'location': [
+					l.id_copie.id_copie for l in client.locations
+				],
+				'forfait': {'max_location' : client.id_abonnement.id_forfait.max_location}
+			},
+			'listePersonne': [
+				{
+					'id_personne': p.id_personne,
+					'nom': p.nom
+				}
+				for p in personnes
+			],
+			'listeActeur': [
+				{
+					'id_personne': a.id_personne,
+					'nom': a.nom,
+					'date_naissance': a.date_naissance.strftime('%Y-%m-%d') if a.date_naissance is not None else None,
+					
+				}
+				for a in acteurs
+			],
+			'listeFilm': [
+				{
+					'id_film': f.id_film,
+					'titre': f.titre,
+					'annee_sortie': f.annee_sortie,
+					'duree': f.duree,
+					'resume': f.resume,
+					'genre': [g.nom for g in f.genre],
+					'pays': [p.nom for p in f.pays],
+					'langue_originale': f.langue_originale,
+					'realisateur': f.id_realisateur.id_personne if f.id_realisateur is not None else None,
+					'scenariste': [s.id_personne for s in f.scenaristes],
+					'acteur': [{'id_personne': r.id_personne.id_personne, 'role': r.titre} for r in f.roles]
+					
+				}
+				for f in films
+			]
+		})
+
+@app.post('/location')
+@orm.db_session
+def location():
+	copie = request.json['id_copie']
+	client = request.json['id_client']
+	
+	theClient = orm.select(c for c in entities.Client if c.id_client == client).first()
+	
+	l = entities.Location(
+		id_client = theClient,
+		id_copie = orm.select(c for c in entities.Copie if c.id_copie == copie).first(),
+		debut = date.today(),
+		fin = date(2018, 3, 29)
+	)
+	
+	print(l)
+	
+	orm.flush()
+	
+	from bottle import response
+	response.content_type = 'application/json'
+	return json.JSONEncoder().encode({
+		'client': {
+			'location': [
+				l.id_copie.id_copie for l in theClient.locations
+			]
+		}
+	})
+
+run(app, host='localhost', port=8090)
