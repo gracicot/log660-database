73cafaf3edd900d1bbd411006d19b2bbcd3f4e1d
MarcOfQc <mo.gagnon.qc@gmail.com>
2018-04-18 12:23:16 -0400

    Changed backend to receive json from frontend

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ entities.init()
 @orm.db_session
 def login():
 	from bottle import response
-	email = request.json['identifiant']
-	password = request.json['motDePasse']
+	requestJson = json.loads(request.body.read())
+	email = requestJson['identifiant']
+	password = requestJson['motDePasse']
 	
 	client = orm.select(c.client for c in entities.AuthentificationClient if c.courriel == email and c.mot_de_passe == password).first()
 	
@@ -36,39 +37,41 @@ def login():
 				],
 				'forfait': {'max_location' : client.id_abonnement.id_forfait.max_location}
 			},
-			'listePersonne': [
-				{
-					'id_personne': p.id_personne,
-					'nom': p.nom
-				}
-				for p in personnes
-			],
-			'listeActeur': [
-				{
-					'id_personne': a.id_personne,
-					'nom': a.nom,
-					'date_naissance': a.date_naissance.strftime('%Y-%m-%d') if a.date_naissance is not None else None,
-					
-				}
-				for a in acteurs
-			],
-			'listeFilm': [
-				{
-					'id_film': f.id_film,
-					'titre': f.titre,
-					'annee_sortie': f.annee_sortie,
-					'duree': f.duree,
-					'resume': f.resume,
-					'genre': [g.nom for g in f.genre],
-					'pays': [p.nom for p in f.pays],
-					'langue_originale': f.langue_originale,
-					'realisateur': f.id_realisateur.id_personne if f.id_realisateur is not None else None,
-					'scenariste': [s.id_personne for s in f.scenaristes],
-					'acteur': [{'id_personne': r.id_personne.id_personne, 'role': r.titre} for r in f.roles]
-					
-				}
-				for f in films
-			]
+			'data': {
+				'listePersonne': [
+					{
+						'id_personne': p.id_personne,
+						'nom': p.nom
+					}
+					for p in personnes
+				],
+				'listeActeur': [
+					{
+						'id_personne': a.id_personne,
+						'nom': a.nom,
+						'date_naissance': a.date_naissance.strftime('%Y-%m-%d') if a.date_naissance is not None else None,
+						
+					}
+					for a in acteurs
+				],
+				'listeFilm': [
+					{
+						'id_film': f.id_film,
+						'titre': f.titre,
+						'annee_sortie': f.annee_sortie,
+						'duree': f.duree,
+						'resume': f.resume,
+						'genre': [g.nom for g in f.genre],
+						'pays': [p.nom for p in f.pays],
+						'langue_originale': f.langue_originale,
+						'realisateur': f.id_realisateur.id_personne if f.id_realisateur is not None else None,
+						'scenariste': [s.id_personne for s in f.scenaristes],
+						'acteur': [{'id_personne': r.id_personne.id_personne, 'role': r.titre} for r in f.roles]
+						
+					}
+					for f in films
+				]
+			}
 		})
 
 @app.post('/location')
@@ -114,9 +117,10 @@ def getFilmCorr(ids, rate):
 
 @app.post('/recommandation')
 def recommandation():
-	film = request.json['id_film']
-	client = request.json['id_client']
-	
+	requestJson = json.loads(request.body.read())
+	film = int(requestJson['id_film'])
+	client = int(requestJson['id_client'])
+
 	conn = sqlite3.connect('./db/Labo2.db')
 	
 	conn.enable_load_extension(True)
@@ -128,7 +132,7 @@ def recommandation():
 	for id in c.execute('''SELECT otherFilmid as idFilm, Correlation.correlation FROM Correlation WHERE thisFilmid = ? AND idFilm NOT IN (SELECT Copie.id_film FROM Location JOIN Copie ON Copie.id_copie = Location.id_copie WHERE Location.id_client = ?) ORDER BY Correlation.correlation LIMIT 3''', [film, client])
 	]
 	
-	c.execute('''SELECT avgcote FROM avgCote WHERE idFilm = ?''', film)
+	c.execute('''SELECT avgcote FROM avgCote WHERE idFilm = ?''', [film])
 	rateobj = c.fetchone()
 	if rateobj is not None:
 		rate = c.fetchone()[0]
